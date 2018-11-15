2246133cb52dbd168ca92115615f745c6b7a9d07
ChrisMiuchiz <chris000005@gmail.com>
2018-11-15 13:51:13 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,41 +1,25 @@
-#include "main.h"
 #include <iostream>
 #include <fstream>
+#include <windows.h>
+#include "callbacks.h"
 
-UINT_PTR base;
+unsigned int base;
 float multiplier = 1.0;
-float current;
-float amount_to_add;
 float maxcrafting = 1.0;
 
-void DLL_EXPORT CraftingMath(){
-    asm("movss _current, xmm0");
-    asm("movss _amount_to_add, xmm1");
+unsigned int CraftingProgressInjection_JMP_back;
+__declspec(naked) __declspec(dllexport) void  CraftingProgressInjection(){
+    //Just apply the multiplier
+    asm("mulss xmm0, [_multiplier]");
 
-    if (multiplier <= 1.3){
-        multiplier *= 1.001;
-    }
+    asm("addss xmm0, xmm1"); //original code
+    asm("comiss xmm0, [_maxcrafting]");
 
-    amount_to_add *= multiplier;
-
-    asm("movss xmm1, [_amount_to_add]");
-    asm("movss xmm0, [_current]");
-
-}
-DWORD CraftingMathPtr = (DWORD)&CraftingMath;
-
-_declspec(naked) void DLL_EXPORT CraftingProgressInjection(){
-
-    asm("call [_CraftingMathPtr]");
-    asm("addss xmm0, xmm1");
-
-    asm("mov ecx, [_base]"); //jump back
-    asm("add ecx, 0x8F20E");
-    asm("comiss xmm0, [_maxcrafting]");//original comparison
-    asm("jmp ecx");
+    asm("jmp [_CraftingProgressInjection_JMP_back]");
 }
 
-_declspec(naked) void DLL_EXPORT CraftingDone(){
+//When the cursor leaves the button.
+__declspec(naked) void __declspec(dllexport) CraftingDone(){
     asm("mov dword ptr _multiplier, 0x3F800000"); //reset multiplier
     asm("mov byte ptr [edi + 0x3C4], 0x0");
 
@@ -45,28 +29,46 @@ _declspec(naked) void DLL_EXPORT CraftingDone(){
 
 }
 
-void WriteJMP(BYTE* location, BYTE* newFunction){
-	DWORD dwOldProtection;
-	VirtualProtect(location, 5, PAGE_EXECUTE_READWRITE, &dwOldProtection);
-    location[0] = 0xE9; //jmp
-    *((DWORD*)(location + 1)) = (DWORD)(( (unsigned INT32)newFunction - (unsigned INT32)location ) - 5);
-	VirtualProtect(location, 5, dwOldProtection, &dwOldProtection);
+__stdcall void __attribute__((noinline)) __declspec(dllexport) HandleFinishCrafting(){
+    /*
+    This seems to be the correct calculation.
+    Wollay says it increases by 20% each time, and this max speed seems to be correct.
+    */
+    if (multiplier < 5.0){
+        multiplier *= 1.20;
+    }
 }
 
 
-extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+void WriteJMP(BYTE* location, BYTE* newFunction){
+    DWORD dwOldProtection;
+    VirtualProtect(location, 5, PAGE_EXECUTE_READWRITE, &dwOldProtection);
+    location[0] = 0xE9; //jmp
+    *((DWORD*)(location + 1)) = (DWORD)(( (unsigned INT32)newFunction - (unsigned INT32)location ) - 5);
+    VirtualProtect(location, 5, dwOldProtection, &dwOldProtection);
+}
+
+
+DWORD WINAPI __attribute__((noinline)) __declspec(dllexport) RegisterCallbacks(){
+        RegisterCallback("RegisterFinishCraftingCallback", HandleFinishCrafting);
+        return 0;
+}
+
+
+
+extern "C" __declspec(dllexport) bool APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-    base = (UINT_PTR)GetModuleHandle(NULL);
+    base = (unsigned int)GetModuleHandle(NULL);
     switch (fdwReason)
     {
 
         case DLL_PROCESS_ATTACH:
-
+            CraftingProgressInjection_JMP_back = base + 0x8F20E;
             WriteJMP((BYTE*)(base + 0x8F203), (BYTE*)&CraftingProgressInjection);
-            WriteJMP((BYTE*)(base + 0x3470A), (BYTE*)&CraftingDone);
 
+            WriteJMP((BYTE*)(base + 0x3470A), (BYTE*)&CraftingDone);
+            CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RegisterCallbacks, 0, 0, NULL);
             break;
-;
     }
-    return TRUE;
+    return true;
 }
